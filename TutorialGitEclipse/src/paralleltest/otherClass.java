ca5fd4ab5edc0d2f5fcc5ff9e86b6e687269fbd2
ricardo <neira.ricardo@gmail.com>
2015-10-20 10:52:43 -0400

    new filed added

---
+++ TutorialGitEclipse/src/paralleltest/otherClass.java
@@ -0,0 +1,15 @@
+package paralleltest;
+
+public class otherClass {
+
+	public otherClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println();
+  System.out.println();
+	}
+
+}
